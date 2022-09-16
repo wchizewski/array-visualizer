da6fdfdcddcebf809ef46aaadbcbee9f3eb4e9a5
wchizewski <90284175+wchizewski@users.noreply.github.com>
2022-09-16 14:47:32 -0600

    video 5

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
-console.log("hi");
+// Array visualizer
+
+// HTML Variables
+let containerEl = document.getElementById("container");
+
+// Global Variable
+let myData = [];
+for (let n = 1; n <= 50; n++) {
+    myData.push(Math.random() * 600);
+}
+
+// Display Data
+drawArray();
+
+function drawArray() {
+    let outputStr = "";
+    for (let i = 0; i < myData.length; i++) {
+        outputStr += `<div style="height:${myData[i]}px"></div>`
+    }
+    containerEl.innerHTML = outputStr;
+}
+
+// Key Event Stuff
+document.addEventListener("keydown", keydownHandler)
+
+function keydownHandler(event) {
+    // modify array
+    for (let i = 0; i < myData.length; i++) {
+        myData[i] = 400;
+    }
+    myData[0] += 10;
+
+    // Redraw the array
+    drawArray();
+}
